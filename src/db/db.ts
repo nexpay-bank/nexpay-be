fd502f4435d8e2b343e58deb52e6988c78151a72
AdonNeet <baradontap@gmail.com>
2025-06-04 22:32:42 +0700

    chore: fix the db

--- src/db/db.ts
+++ src/db/db.ts
@@ -5,7 +5,7 @@ import postgres from 'postgres'
 
 async function main() {
     // Disable prefetch as it is not supported for "Transaction" pool mode 
-    const client = postgres(process.env.DATABASE_URL, { prepare: false })
+    const client = postgres(process.env.DATABASE_URL!, { prepare: false })
     const db = drizzle({ client });
 }
 
