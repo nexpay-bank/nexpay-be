93f2ff7b748e1aa66cb89358ed13813063ed5cb0
AdonNeet <baradontap@gmail.com>
2025-06-08 20:09:43 +0700

    chores: back to listener (old way)

--- src/server.ts
+++ src/server.ts
@@ -1,50 +1,62 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// src/server.ts
 import * as dotenv from "dotenv";
 dotenv.config();
 
 import Hapi, { Request, ResponseToolkit } from "@hapi/hapi";
 import { routes } from "./routes/apiRoutes";
 
-const server = Hapi.server({
-  host: "0.0.0.0",
-  port: 3000,
-  routes: {
-    cors: {
-      origin: ["*"],
-      additionalHeaders: ["x-api-key", "content-type", "authorization"],
-      additionalExposedHeaders: ["x-api-key"],
+const init = async () => {
+  const server = Hapi.server({
+    host: "0.0.0.0",
+    port: 3000,
+    routes: {
+      cors: {
+        origin: ["*"],
+        additionalHeaders: ["x-api-key", "content-type", "authorization"],
+        additionalExposedHeaders: ["x-api-key"],
+      },
     },
-  },
-});
+  });
 
-server.route(routes);
+  server.route(routes);
 
-const VALID_API_KEY = process.env.KunciRumah;
+  const VALID_API_KEY = process.env.KunciRumah;
 
-server.ext("onRequest", (request: Request, h: ResponseToolkit) => {
-  console.log(
-    `Incoming request: ${request.method.toUpperCase()} ${request.path}`
-  );
+  server.ext("onRequest", (request: Request, h: ResponseToolkit) => {
+    console.log(
+      `Incoming request: ${request.method.toUpperCase()} ${request.path}`
+    );
 
-  if (request.method === "options") {
+    if (request.method === "options") {
+      return h.continue;
+    }
+
+    const apiKey = request.headers["x-api-key"];
+
+    if (!apiKey || apiKey !== VALID_API_KEY) {
+      return h.response({ error: "Invalid API Key" }).code(403).takeover();
+    }
+
     return h.continue;
-  }
+  });
 
-  const apiKey = request.headers["x-api-key"];
+  server.ext("onPreResponse", (request: Request, h: ResponseToolkit) => {
+    const response = request.response;
+    // Cek jika ada error dari Boom (error handler-nya Hapi)
+    if ((response as any).isBoom) {
+      console.error("Error occurred:", (response as any).output.payload);
+    }
+    return h.continue;
+  });
 
-  if (!apiKey || apiKey !== VALID_API_KEY) {
-    return h.response({ error: "Invalid API Key" }).code(403).takeover();
-  }
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
 
-  return h.continue;
+process.on("unhandledRejection", (err: any) => {
+  console.log(err);
+  process.exit(1);
 });
 
-server.ext("onPreResponse", (request: Request, h: ResponseToolkit) => {
-  const response = request.response;
-  if ((response as any).isBoom) {
-    console.error("Error occurred:", (response as any).output.payload);
-  }
-  return h.continue;
-});
-
-// Export handler buat Vercel
-export default server.listener;
+init();
