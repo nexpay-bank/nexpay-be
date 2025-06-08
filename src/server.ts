7dd4bdeef2288a2e5a57c7230cb1583717181a1f
AdonNeet <baradontap@gmail.com>
2025-06-08 20:16:24 +0700

    chores: vercel probe

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import { routes } from "./routes/apiRoutes";
 const init = async () => {
   const server = Hapi.server({
     host: "0.0.0.0",
-    port: 3000,
+    port: process.env.PORT || 3000,
     routes: {
       cors: {
         origin: ["*"],
